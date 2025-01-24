a3713fb71ff76541f81a64efeba3ba0ebbf2888b
Veera Pallati <pkondalu@gmail.com>
2025-01-24 21:23:49 +0530

    Organizing the folder structure.

--- frontend/src/authentication/auth-actions.js
+++ frontend/src/authentication/auth-actions.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut   } from "firebase/auth";
-import { auth } from "../firebase-config";
+import { auth } from "../config/firebase-config";
 
 export const signUp = async (email, password) => {
     try {
--- frontend/src/authentication/auth-context.js
+++ frontend/src/authentication/auth-context.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase-config";
+import { auth } from "../config/firebase-config";
 
 const AuthContext = createContext();
 
