ccdad4a4e0743c9f4d9b20ed8c14f2b8a0a49859
Veera Pallati <pkondalu@gmail.com>
2025-02-01 06:48:51 +0530

    Adding Verify Token to all the authenticated endpoints.

--- functions/src/routes/smsRoutes.js
+++ functions/src/routes/smsRoutes.js
@@ -4,10 +4,10 @@ const { verifyToken } = require('../middleware/VerifyToken');
 
 const router = express.Router();
 
-router.post('/send', smsController.sendSms);
-router.post('/queue', smsController.queueSms);
-router.get('/logs', smsController.readMessageLogs);
-router.post('/importMessageLogs', smsController.importMessageLogs);
-router.get('/messageLogs', verifyToken , smsController.readMessageLogsFromDb);
+router.post('/send', verifyToken, smsController.sendSms);
+router.post('/queue', verifyToken, smsController.queueSms);
+router.get('/logs', verifyToken, smsController.readMessageLogs);
+router.post('/importMessageLogs', verifyToken, smsController.importMessageLogs);
+router.get('/messageLogs', verifyToken, smsController.readMessageLogsFromDb);
 
 module.exports = router;
