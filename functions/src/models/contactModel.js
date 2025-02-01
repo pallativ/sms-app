489c23519048714ab225bef06fedaf8aca2e61e5
Veera Pallati <pkondalu@gmail.com>
2025-02-01 07:10:57 +0530

    Update all the contact service and contact models using users collection.

--- functions/src/models/contactModel.js
+++ functions/src/models/contactModel.js
@@ -1,6 +1,7 @@
 const { db } = require('../../firebaseSetup');
 const { logger } = require('firebase-functions');
 
+/// Get all contacts
 exports.getAllContacts = async function getAllContacts(userEmail) {
     try {
         logger.debug('fetching contacts from contacts collection.');
@@ -16,12 +17,13 @@ exports.getAllContacts = async function getAllContacts(userEmail) {
     }
 }
 
+/// Check if a contact exists
 exports.isExists = async function isExists(userEmail, contactEmail) {
     const doc = await db.collection("users").doc(userEmail).collection("contacts").doc(contactEmail).get();
     return doc.exists;
 }
 
-
+/// Create a contact
 exports.createContact = async function createContact(owner, contact) {
     try {
 
@@ -41,9 +43,10 @@ exports.createContact = async function createContact(owner, contact) {
     }
 }
 
-exports.getContactById = async function getContactById(contactId) {
+/// Get a contact by id
+exports.getContactById = async function getContactById(userEmail, contactId) {
     try {
-        const doc = await db.collection('contacts').doc(contactId).get();
+        const doc = await db.collection('users').doc(userEmail).collection('contacts').doc(contactId).get();
         if (!doc.exists) {
             throw new Error('No such contact!');
         }
@@ -53,18 +56,20 @@ exports.getContactById = async function getContactById(contactId) {
     }
 }
 
-exports.updateContact = async function updateContact(contactId, contact) {
+/// Update a contact
+exports.updateContact = async function updateContact(userEmail, contactId, contact) {
     try {
-        await db.collection('contacts').doc(contactId).update(contact);
+        await db.collection('users').doc(userEmail).collection('contacts').doc(contactId).update(contact);
         return true;
     } catch (error) {
         throw new Error('Error updating contact: ' + error.message);
     }
 }
 
-exports.deleteContact = async function deleteContact(contactId) {
+// Delete a contact
+exports.deleteContact = async function deleteContact(userEmail, contactId) {
     try {
-        await db.collection('contacts').doc(contactId).delete();
+        await db.collection('users').doc(userEmail).collection('contacts').doc(contactId).delete();
         return true;
     } catch (error) {
         throw new Error('Error deleting contact: ' + error.message);
