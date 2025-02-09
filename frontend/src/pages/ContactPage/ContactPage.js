f417e2eb12a6c9c965a88e1e95f0acbd1742220b
HARIGUTTI2002 <harigutti2002@gmail.com>
2025-02-09 15:32:47 +0530

    Added images frocontact and user
    >  contact and list are set

--- frontend/src/pages/ContactPage/ContactPage.js
+++ frontend/src/pages/ContactPage/ContactPage.js
@@ -1,14 +1,16 @@
 import React from "react";
-import '../../styles/syncfusion'
-import CreateContact from "../../components/Contacts/AddContact";
+import "../../styles/syncfusion";
+import CreateContact from "../../components/Contacts/CreateContact";
+import ContactList from "../../components/Contacts/ContactList";
 
 const ContactsPage = () => {
     return (
         <React.Fragment>
             <h1>Contacts</h1>
             <p>Welcome to the Contacts Page.</p>
-            <div style={{ width: 400}}>
-            <CreateContact />
+
+            <div>
+                <ContactList />
             </div>
         </React.Fragment>
     );
--- frontend/src/pages/SmsPage/SmsPage.js
+++ frontend/src/pages/SmsPage/SmsPage.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import SelectContact from "../../components/Contacts/SelectContact";
 
 const SmsPage = () => {
     return (
-        <div>
-            <h1>SMS Page</h1>
-            <p>Welcome to the SMS Page. Here you can manage your SMS messages.</p>
-        </div>
+        <>
+            <div>
+                <h1>SMS Page</h1>
+                <p>
+                    Welcome to the SMS Page. Here you can manage your SMS
+                    messages.
+                </p>
+            </div>
+            <div>
+                <SelectContact />
+            </div>
+        </>
     );
 };
 
