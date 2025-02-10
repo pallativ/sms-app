cbf9925ebe80a53239296ac80fbdd0388fc1d487
HARIGUTTI2002 <harigutti2002@gmail.com>
2025-02-10 15:13:46 +0530

    added
    initgration with select contact and message body componets

--- frontend/src/pages/WhatsupPage/WhatsupPage.js
+++ frontend/src/pages/WhatsupPage/WhatsupPage.js
@@ -1,73 +1,17 @@
-import React from "react";
-import {
-    HtmlEditor,
-    Image,
-    Inject,
-    Link,
-    QuickToolbar,
-    RichTextEditorComponent,
-    Toolbar,
-    EmojiPicker,
-} from "@syncfusion/ej2-react-richtexteditor";
+import React, { useState } from "react";
+import "../../styles/syncfusion";
+import MessageBody from "../../components/Sms/MessageBody";
+import SelectContact from "../../components/Contacts/SelectContact";
 
 const WhatsupPage = () => {
-    const toolbarSettings = {
-        items: [
-            "Bold",
-            "Italic",
-            "Underline",
-            "StrikeThrough",
-            "FontName",
-            "FontSize",
-            "FontColor",
-            "BackgroundColor",
-            "LowerCase",
-            "UpperCase",
-            "|",
-            "Formats",
-            "Alignments",
-            "OrderedList",
-            "UnorderedList",
-            "Outdent",
-            "Indent",
-            "|",
-            "CreateLink",
-            "Image",
-            "EmojiPicker", // Make sure EmojiPicker is added
-            "|",
-            "ClearFormat",
-            "Print",
-            "SourceCode",
-            "FullScreen",
-            "|",
-            "Undo",
-            "Redo",
-        ],
-    };
-
+    const [selectedPhone, setSelectedPhone] = useState(""); // Store selected phone
     return (
-        <>
-            <div>
-                <h2>Send a message</h2>
-            </div>
-            <div style={{ width: "100%", height: "auto" }}>
-                <RichTextEditorComponent
-                    height={250}
-                    toolbarSettings={toolbarSettings}
-                >
-                    <Inject
-                        services={[
-                            Toolbar,
-                            Image,
-                            Link,
-                            HtmlEditor,
-                            QuickToolbar,
-                            EmojiPicker,
-                        ]}
-                    />
-                </RichTextEditorComponent>
-            </div>
-        </>
+        <React.Fragment>
+            <h1>WhatsupPage</h1>
+            <p>Welcome to the WhatsupPage.</p>
+            <SelectContact setSelectedPhone={setSelectedPhone} />
+            <MessageBody selectedPhone={selectedPhone} />
+        </React.Fragment>
     );
 };
 
