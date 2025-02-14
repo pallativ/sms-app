4448127f7be612d51d4cf2f98ed8ff319d8136b0
Veera Pallati <pkondalu@gmail.com>
2025-02-14 10:33:00 +0530

    adding the check to verify the logged user is a super admin for creating the tenant.

--- functions/src/controllers/TenantController.js
+++ functions/src/controllers/TenantController.js
@@ -5,13 +5,18 @@ const { auth } = require('../../firebaseSetup');
 exports.createTenant = async (req, res) => {
     try {
         const { code, name, adminEmail } = req.body;
-        if (await tenantModel.checkTenantExists(code)) {
-            res.status(400).json({ message: `Tenant already exists`, tenantCode: code, name: name });
+        if (req.user.role === "super-admin") {
+            if (await tenantModel.checkTenantExists(code)) {
+                res.status(400).json({ message: `Tenant already exists`, tenantCode: code, name: name });
+            }
+            else {
+                const tenantDetails = await tenantService.createTenant({ code, name, adminEmail: adminEmail });
+                console.log(tenantDetails)
+                res.status(201).json({ message: 'Tenant created successfully.', id: tenantDetails.tenantId });
+            }
         }
         else {
-            const tenantDetails = await tenantService.createTenant({ code, name, adminEmail: adminEmail });
-            console.log(tenantDetails)
-            res.status(201).json({ message: 'Tenant created successfully.', id: tenantDetails.tenantId });
+            res.status(403).json({ message: 'Your are not authorized to create the tenant' });
         }
     } catch (error) {
         res.status(500).json({ error: error });
@@ -22,10 +27,11 @@ exports.addTenantAdmin = async (req, res) => {
     try {
         if (req.user.email === "admin@msgrouter.in") {
             console.log("Adding Claims to admin user");
-            setCustomClaim(req.user.uid, "tenant-admin");
+            setCustomClaim(req.user.uid, "super-admin");
             res.status(200).json({ message: 'current logged in user set as tenant admin', email: req.user.email });
         }
-        res.status(400).json({message: "Tenant Admin is not allowed for this user.", email: req.user.email});
+        else
+            res.status(400).json({ message: "Tenant Admin is not allowed for this user.", email: req.user.email });
     } catch (error) {
         res.status(500).json({ error: error });
     }
