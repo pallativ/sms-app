c3defd42a7213f38edeeaeaadf8e1e22530918f0
Veera Pallati <pkondalu@gmail.com>
2025-02-16 11:44:05 +0530

    Adding impersonate tenant admin.

--- functions/src/services/tenantService.js
+++ functions/src/services/tenantService.js
@@ -56,6 +56,16 @@ exports.getUsersByTenant = async (tenantCode) => {
     }
 }
 
+exports.impersonateSuperAdmin = async (userInfo, tenantCode) => {
+    try {
+        // Assigning the claims to impersonate tenant Admin.
+        await tenantModel.setCustomClaimByName(userInfo.uid, "tenantCode", [tenantCode]);
+        return { message: "Impersonation successful", uid: userRecord.uid };
+    } catch (error) {
+        logger.error('Error in impersonating tenant admin', error);
+        throw new Error("Error in impersonating tenant admin");
+    }
+}
 
 async function getUserByEmail(email) {
     try {
@@ -67,3 +77,5 @@ async function getUserByEmail(email) {
         return null;
     }
 }
+
+
